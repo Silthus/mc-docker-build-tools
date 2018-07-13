b00ac461f51849e7374a9c730f8f4b103a3ed9ba
Michael Reichenbach <michael.reichenbach@interhyp.de>
2018-07-13 15:03:36 +0200

    remove .idea/ folder from git

--- src/main/java/de/faldoria/buildtools/Builder.java
+++ src/main/java/de/faldoria/buildtools/Builder.java
@@ -197,7 +197,7 @@ public class Builder
                 System.out.println( "*** Please note that this is a beta feature, so if it does not work please also try a manual install of git from https://git-for-windows.github.io/ ***" );
             } else
             {
-                ex.printStackTrace();
+                System.err.println(ex.getMessage());
                 System.out.println( "You must run this jar through bash (msysgit)" );
                 System.exit( 1 );
             }
